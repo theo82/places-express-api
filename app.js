7a2e50901affd799c56a7446ed599b67fb9ed462
theo82 <theoaristi53@gmail.com>
2020-10-18 09:13:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+app.use(placesRoutes);
+
+
+app.listen(5000, () => {
+    console.log('App listening on port 5000!');
+});
