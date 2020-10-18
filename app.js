5d59640aa120de6feeabe86471bc0d49557ba042
theo82 <theoaristi53@gmail.com>
2020-10-18 10:23:43 +0300

    Adding Place-Specific Routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
-app.use(placesRoutes);
+app.use('/api/places',placesRoutes);
 
 
 app.listen(5000, () => {
