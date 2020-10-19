7e56a21fa2230a66655727127b1c6c75ced5deb5
theo82 <theoaristi53@gmail.com>
2020-10-19 19:04:25 +0300

    Adding a POST Route and Using Postman

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -40,5 +40,22 @@ const getPlaceByUserId = (req, res, next) => {
     res.json({ place });
 }
 
+const createPlace = (req, res, next) => {
+    const { title, description, coordinates, adress, creator } = req.body;
+    const {v4 : uuidv4} = require('uuid');
+    const createdPlace = {
+        id: uuidv4(),
+        title,
+        description,
+        location: coordinates,
+        adress,
+        creator
+    };
+    DUMMY_PLACES.push(createdPlace);
+
+    res.status(201).json({place: createdPlace});
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
