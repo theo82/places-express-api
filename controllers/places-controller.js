e47d417523e453fc854feb11a753ffaf795bdef7
theo82 <theoaristi53@gmail.com>
2020-10-20 13:19:36 +0300

    Deleting Places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,6 +1,6 @@
 const HttpError = require("../models/http-error");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
       id: 'p1',
       title: 'Empire State Building',
@@ -72,7 +72,9 @@ const updatePlace = (req, res, next) => {
 };
 
 const deletePlace = (req, res, next) => {
-
+    const placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+    res.status(200).json({message: 'Deleted place.'});
 }
 
 exports.getPlaceById = getPlaceById;
