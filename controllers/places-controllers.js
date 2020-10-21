b87935a381ede67fc663ee9e79345369f0befb1a
theo82 <theoaristi53@gmail.com>
2020-10-21 09:03:37 +0300

    Validating API Input (Request

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const { validationResult } = require('express-validator');
 
 let DUMMY_PLACES = [
     {
@@ -43,6 +44,14 @@ const getPlacesByUserId = (req, res, next) => {
 }
 
 const createPlace = (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        console.log(errors);
+        throw new HttpError('Invalid inputs passed, please check your data', 422);
+    }
+
     const { title, description, coordinates, adress, creator } = req.body;
     const {v4 : uuidv4} = require('uuid');
     const createdPlace = {
