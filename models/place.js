3d28f44bd5c1253bff4d8f7984ac516a350e7abb
theo82 <theoaristi53@gmail.com>
2020-10-24 13:23:24 +0300

    Creating & Storing Documents

---
+++ models/place.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const placesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    location: {
+        lat: { type: Number, required: true},
+        lng: { type: Number, required: true},
+    },
+    creator: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = mongoose.model('Places', placesSchema);
