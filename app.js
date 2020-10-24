35abbf45ddec2a736082bd1b135536d1f73b2a0a
theo82 <theoaristi53@gmail.com>
2020-10-24 11:47:55 +0300

    Creating the Place Schema &

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -26,7 +27,11 @@ app.use((error, req, res, next) => {
 
 })
 
-
-app.listen(5000, () => {
-    console.log('App listening on port 5000!');
+mongoose.connect('mongodb+srv://theoaristi53:I2YjAHwxnKfB0Ysq@cluster0.1o6wa.mongodb.net/places?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(5000);
+})
+.catch(err => {
+    console.log(err)
 });
+
