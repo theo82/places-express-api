60407b57bcef90c5d2395b2f227f28cd359f1c2a
theo82 <theoaristi53@gmail.com>
2020-10-25 07:08:56 +0200

    Getting Places by the PlaceID

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -16,17 +16,19 @@ let DUMMY_PLACES = [
     }
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async (req, res, next) => {
     const placeId = req.params.pid;
 
-    const place = DUMMY_PLACES.find((p) => {
-        return p.id === placeId;
-    });
+    let place;
 
-    if(!place) {
-        throw new HttpError('Could not find a place for the provided id.', 404);
+    try{
+      place = await Place.findById(placeId);
+    } catch(err) {
+        const error =  new HttpError('Something went wrong, could not find a place.', 404);
+        return next(error);
     }
-    res.json({ place });
+   
+    res.json({ place: place.toObject({ getters: true }) });
 }
 
 const getPlacesByUserId = (req, res, next) => {
