d9732677bedc5bd41e791fcd2b71e3b1535d170d
theo82 <theoaristi53@gmail.com>
2020-10-26 07:05:48 +0200

    Adding the User Login

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -108,7 +108,9 @@ const updatePlace = async (req, res, next) => {
 
     if(!errors.isEmpty()) {
         console.log(errors);
-        throw new HttpError('Invalid inputs passed, please check your data', 422);
+        return next(
+            new HttpError('Invalid inputs passed, please check your data', 422)
+        );
     }
 
     const { title, description } = req.body;
