0d6cefdefe6a7ef06bc88e5664a185676a17b4ab
theo82 <theoaristi53@gmail.com>
2020-11-03 09:28:38 +0200

    Connecting Users to Images

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,6 +1,5 @@
 const multer = require('multer');
-const uuid = require('uuid/v1');
-
+const {v4 : uuidv4} = require('uuid');
 const MIME_TYPE_MAP = {
     'image/png': 'png',
     'image/jpeg': 'png',
@@ -14,7 +13,7 @@ const fileUpload = multer({
         },
         filename: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype]
-            cb(null, uuid() + '.' + ext);
+            cb(null, uuidv4() + '.' + ext);
         }
     }),
     fileFilter: (req, file, cb) => {
